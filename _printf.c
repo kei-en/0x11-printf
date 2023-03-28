24acdfc56e20e972a11304628caa8e23066e5b3f
kei-en <njuguna0karanja@gmail.com>
2023-03-28 11:54:48 -0700

    Logic for printing literal strings

--- _printf.c
+++ _printf.c
@@ -9,11 +9,21 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
+	const char *p;
 
 	if (!format)
 		return(-1);
 
 	va_start(args, format);
+	for (p = format; *p != '\0'; p++)
+	{
+		if (*p != '%')
+		{
+			_putchar(*p, &k);
+			continue;
+		}
+		p++;
+	}
 
 	va_end(args);
 }
