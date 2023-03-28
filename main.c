36ff3c6c2e258b7d731c795729f27fec4c298131
kei-en <njuguna0karanja@gmail.com>
2023-03-28 12:50:45 -0700

    More fixes

--- main.c
+++ main.c
@@ -9,10 +9,7 @@
  */
 int main(void)
 {
-	int len, len2;
 
-	len = _printf("Let's try to printf a simple sentence.\n");
-	len2 = printf("Let's try to printf a simple sentence.\n");
 	_printf("String:[%s]\n", "I am a string !");
 
 	_printf("Character: [%c]\n", 'H');
